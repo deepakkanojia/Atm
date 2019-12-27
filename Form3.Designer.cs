e627b10beca842a7098d2d04ba0bd92c670cb9f8
deepakkanojia <kanojiadeepak401@gmail.com>
2019-12-27 19:18:59 +0530

    Repository no 3
    Atm Module

---
+++ Form3.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace atm
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.quickcash = new System.Windows.Forms.Button();
+            this.banking = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(263, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "enter your choice";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "services";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "mini statement";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // quickcash
+            // 
+            this.quickcash.Location = new System.Drawing.Point(32, 230);
+            this.quickcash.Name = "quickcash";
+            this.quickcash.Size = new System.Drawing.Size(88, 23);
+            this.quickcash.TabIndex = 3;
+            this.quickcash.Text = "quick cash";
+            this.quickcash.UseVisualStyleBackColor = true;
+            this.quickcash.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // banking
+            // 
+            this.banking.Location = new System.Drawing.Point(390, 115);
+            this.banking.Name = "banking";
+            this.banking.Size = new System.Drawing.Size(75, 23);
+            this.banking.TabIndex = 4;
+            this.banking.Text = "banking";
+            this.banking.UseVisualStyleBackColor = true;
+            this.banking.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(390, 171);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "balance inq";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(390, 230);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "transfer";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 340);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.banking);
+            this.Controls.Add(this.quickcash);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button quickcash;
+        private System.Windows.Forms.Button banking;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
---
+++ banking.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace atm
+{
+    partial class banking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.saving = new System.Windows.Forms.Button();
+            this.current = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(231, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "type of account";
+            // 
+            // saving
+            // 
+            this.saving.Location = new System.Drawing.Point(42, 113);
+            this.saving.Name = "saving";
+            this.saving.Size = new System.Drawing.Size(75, 23);
+            this.saving.TabIndex = 1;
+            this.saving.Text = "saving";
+            this.saving.UseVisualStyleBackColor = true;
+            // 
+            // current
+            // 
+            this.current.Location = new System.Drawing.Point(399, 113);
+            this.current.Name = "current";
+            this.current.Size = new System.Drawing.Size(75, 23);
+            this.current.TabIndex = 2;
+            this.current.Text = "current";
+            this.current.UseVisualStyleBackColor = true;
+            this.current.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // banking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 306);
+            this.Controls.Add(this.current);
+            this.Controls.Add(this.saving);
+            this.Controls.Add(this.label1);
+            this.Name = "banking";
+            this.Text = "banking";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button saving;
+        private System.Windows.Forms.Button current;
+    }
+}
